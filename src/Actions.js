4215fba14339a115509ddf214bd4b01d1b1015d8
F.shk <fshk8795299974@gmail.com>
2025-05-02 18:07:42 +0530

    Update Actions.js

--- src/Actions.js
+++ src/Actions.js
@@ -1,10 +1,23 @@
 const ACTIONS = {
+    // Existing actions
     JOIN: 'join',
     JOINED: 'joined',
     DISCONNECTED: 'disconnected',
     CODE_CHANGE: 'code-change',
     SYNC_CODE: 'sync-code',
     LEAVE: 'leave',
+    
+    // New file management actions
+    CREATE_FILE: 'create-file',
+    FILE_CREATED: 'file-created',
+    DELETE_FILE: 'delete-file',
+    FILE_DELETED: 'file-deleted',
+    RENAME_FILE: 'rename-file',
+    FILE_RENAMED: 'file-renamed',
+    SWITCH_FILE: 'switch-file',
+    FILE_SWITCHED: 'file-switched',
+    SYNC_FILES: 'sync-files',
+    FILES_SYNCED: 'files-synced'
 };
 
 module.exports = ACTIONS;
